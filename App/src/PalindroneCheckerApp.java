81ca7829b1f2dd532cb67ce03001de3d931c87df
kollekrishnakarthik <kolle.krishnakarthik@gmail.com>
2026-03-05 09:43:48 +0530

    UC6: Queue + Stack Based Palindrome Check

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,22 +1,56 @@
+import java.util.Queue;
 import java.util.Stack;
+import java.util.LinkedList;
 
-public class PalindroneCheckerStack {
-    public static boolean isPalindrone(String input) {
+public class PalindroneCheckerApp {
+    public static void main(String[] args) {
+        String input = "racecar";
+
+        // Using Queue (FIFO - First In First Out)
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+        }
+
+        // Using Stack (LIFO - Last In First Out)
         Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
+        System.out.println("Input: " + input);
+        System.out.println("\nQueue (FIFO) Output:");
+        while (!queue.isEmpty()) {
+            System.out.print(queue.poll());
+        }
+
+        System.out.println("\nStack (LIFO) Output:");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+
+        // Compare outputs for palindrone validation
+        System.out.println("\n\nPalindrone Check:");
+        Queue<Character> queueCheck = new LinkedList<>();
+        Stack<Character> stackCheck = new Stack<>();
+
         for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
+            queueCheck.add(c);
+            stackCheck.push(c);
+        }
+
+        boolean isPalindrone = true;
+        while (!queueCheck.isEmpty() && !stackCheck.isEmpty()) {
+            if (queueCheck.poll() != stackCheck.pop()) {
+                isPalindrone = false;
+                break;
             }
         }
-        return true;
-    }
 
-    public static void main(String[] args) {
-        String test = "racecar";
-        System.out.println(test + " is palindrone? " + isPalindrone(test));
+        if (isPalindrone) {
+            System.out.println(input + " is a palindrone (Queue and Stack outputs match in reverse)");
+        } else {
+            System.out.println(input + " is not a palindrone");
+        }
     }
 }
